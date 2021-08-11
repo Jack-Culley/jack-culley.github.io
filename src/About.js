b0cb15ed56efaf4639061bf0c3225382f55ca9bc
Jack-Culley <jcu2ley@gmail.com>
2021-08-10 20:40:18 -0400

    attempt 2 at fixing logo error

--- src/About.js
+++ src/About.js
@@ -24,7 +24,7 @@ function About() {
         <h3 className={introVisibility ? 'head' : 'head not'}>Introduction</h3>
         </div>
         <div className='info' ref={introRef}>
-        <img className={introVisibility ? 'right visible' : 'right unvisible'} src='/Files/todo.png' width='200px' height='200px'/>
+        <img className={introVisibility ? 'right visible' : 'right unvisible'} src='todo.png' width='200px' height='200px'/>
         <h6 className={introVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>My name is John, but some people call me Jack. I was born and raised in Columbus, Ohio. I am currently 20
         years old. </h6>
         </div>
@@ -40,7 +40,7 @@ function About() {
         <h6 className={profVisibility ? 'right ab text-box visible' : 'right ab text-box unvisible'}>I graduated high school in the spring of 2019. I chose to attend The Ohio State University where I am
         currently a junior pursuing a bachelors degree in Computer Science and Engineering. I plan to graduate with my
         intended degree in the spring of 2023.</h6>
-        <img className={profVisibility ? 'left visible' : 'left unvisible'} src='Files/todo.png' width='200px' height='200px'/>
+        <img className={profVisibility ? 'left visible' : 'left unvisible'} src='todo.png' width='200px' height='200px'/>
         </div>
       </div>
       <a href="#int" className={profVisibility ? 'plug' : 'plug not'} id='int'>
@@ -51,7 +51,7 @@ function About() {
         <h3 className={intVisibility ? 'head' : 'head not'}>Interests</h3>
         </div>
         <div className='info'  ref={intRef}>
-        <img className={intVisibility ? 'right visible' : 'right unvisible'} src='Files/todo.png' width='200px' height='200px'/>
+        <img className={intVisibility ? 'right visible' : 'right unvisible'} src='todo.png' width='200px' height='200px'/>
         <h6 className={intVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>Outside of school I enjoy spending my time lifting, video-gaming, woodworking, and simply hanging out with
         friends/family. I also enjoy coding(hence my major) and like to learn new technologies for building various applications.
         My immediate interests related to computer science are DATABASE STUFF, and python</h6>

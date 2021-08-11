7182e78f2366c4dffadf97911605dab3b3073944
Jack-Culley <jcu2ley@gmail.com>
2021-08-11 11:21:38 -0400

    few small updates

--- src/About.js
+++ src/About.js
@@ -25,8 +25,9 @@ function About() {
         </div>
         <div className='info' ref={introRef}>
         <img className={introVisibility ? 'right visible' : 'right unvisible'} src='todo.png' width='200px' height='200px'/>
-        <h6 className={introVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>My name is John, but some people call me Jack. I was born and raised in Columbus, Ohio. I am currently 20
-        years old. </h6>
+        <h6 className={introVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>
+        My name is John, but most call me Jack. I was born and raised in Columbus, Ohio. 
+        I am currently 20 years old. </h6>
         </div>
       </div>
       <a href="#prof" className={introVisibility ? 'plug' : 'plug not'} id='prof'>
@@ -37,7 +38,8 @@ function About() {
         <h3 className={profVisibility ? 'head' : 'head not'}>Professional</h3>
         </div>
         <div className='info' ref={profRef}>
-        <h6 className={profVisibility ? 'right ab text-box visible' : 'right ab text-box unvisible'}>I graduated high school in the spring of 2019. I chose to attend The Ohio State University where I am
+        <h6 className={profVisibility ? 'right ab text-box visible' : 'right ab text-box unvisible'}>
+        I graduated high school in the spring of 2019. I chose to attend The Ohio State University where I am
         currently a junior pursuing a bachelors degree in Computer Science and Engineering. I plan to graduate with my
         intended degree in the spring of 2023.</h6>
         <img className={profVisibility ? 'left visible' : 'left unvisible'} src='todo.png' width='200px' height='200px'/>
@@ -52,9 +54,10 @@ function About() {
         </div>
         <div className='info'  ref={intRef}>
         <img className={intVisibility ? 'right visible' : 'right unvisible'} src='todo.png' width='200px' height='200px'/>
-        <h6 className={intVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>Outside of school I enjoy spending my time lifting, video-gaming, woodworking, and simply hanging out with
+        <h6 className={intVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>
+        Outside of school I enjoy spending my time lifting, cycling, video-gaming, woodworking, traveling, and simply hanging out with
         friends/family. I also enjoy coding(hence my major) and like to learn new technologies for building various applications.
-        My immediate interests related to computer science are DATABASE STUFF, and python</h6>
+        My immediate interests related to computer science are backend web development(PHP, SQL, MySQL), and python.</h6>
         </div>
       </div>
       <a href="#proj" className={intVisibility ? 'plug' : 'plug not'} id="proj">

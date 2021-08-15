a77695cc9badb51b0fd704c871dd99c2607467bf
Jack-Culley <jcu2ley@gmail.com>
2021-08-14 20:57:13 -0400

    added about photos

--- src/About.js
+++ src/About.js
@@ -25,22 +25,7 @@ function About() {
   return (
 
     <div className="main" id='about'>
-      <h1 className={introVisibility ? 'head' : 'head not'}>About me</h1>
-
-      <div className={introVisibility ? 'intro vis' : 'intro unvis'}>
-        <div className='heading'>
-        <h3 className={introVisibility ? 'head' : 'head not'}>Current Life</h3>
-        </div>
-        <div className='info' ref={introRef}>
-        <img className={introVisibility ? 'right visible' : 'right unvisible'} src='todo.png' width='200px' height='200px'/>
-        <h6 className={introVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>
-        My name is John, but most call me Jack. I was born and raised in Columbus, Ohio. 
-        I am currently 20 years old. </h6>
-        </div>
-      </div>
-      <a href="#prof" className={introVisibility ? 'plug arrow' : 'plug not arrow'} id='prof'>
-            <img className='ext' src='Files/down-done.png' width="40" height="40"/>
-      </a>
+      <h1 className={profVisibility ? 'head' : 'head not'}>About me</h1>
       <div className={profVisibility ? 'prof vis' : 'prof unvis'}>
         <div className='heading'>
         <h3 className={profVisibility ? 'head' : 'head not'}>Professional</h3>
@@ -50,10 +35,26 @@ function About() {
         I graduated high school in the spring of 2019. I chose to attend The Ohio State University where I am
         currently a junior pursuing a bachelors degree in Computer Science and Engineering. I plan to graduate with my
         intended degree in the spring of 2023.</h6>
-        <img className={profVisibility ? 'left visible' : 'left unvisible'} src='todo.png' width='200px' height='200px'/>
+        <img className={profVisibility ? 'left visible' : 'left unvisible'} src='Files/prof.JPG' width='200px' height='auto'/>
         </div>
       </div>
-      <a href="#int" className={profVisibility ? 'plug arrow' : 'plug not arrow'} id='int'>
+      <a href="#prof" className={profVisibility ? 'plug arrow' : 'plug not arrow'} id='prof'>
+            <img className='ext' src='Files/down-done.png' width="40" height="40"/>
+      </a>
+      <div className={introVisibility ? 'intro vis' : 'intro unvis'}>
+        <div className='heading'>
+        <h3 className={introVisibility ? 'head' : 'head not'}>College Life</h3>
+        </div>
+        <div className='info' ref={introRef}>
+        <img className={introVisibility ? 'right visible' : 'right unvisible'} src='Files/life.JPG' width='250px' height='auto'/>
+        <h6 className={introVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>
+          Outside of the classroom I am involved in Cru student ministries and Beta Upsilon Chi(BYX),
+          a Christian fraternity. In Cru, I lead a small group for one of the dorms on campus. In BYX I am the 
+          Pledge Captain, which involves leading and educating men who wish to join the fraternity. 
+        </h6>
+        </div>
+      </div>
+      <a href="#int" className={introVisibility ? 'plug arrow' : 'plug not arrow'} id='int'>
             <img className='ext' src='Files/down-done.png' width="40" height="40"/>
       </a>
       <div className={intVisibility ? 'interests vis' : 'interests unvis'}>
@@ -61,7 +62,7 @@ function About() {
         <h3 className={intVisibility ? 'head' : 'head not'}>Interests</h3>
         </div>
         <div className='info'  ref={intRef}>
-        <img className={intVisibility ? 'right visible' : 'right unvisible'} src='todo.png' width='200px' height='200px'/>
+        <img className={intVisibility ? 'right visible' : 'right unvisible'} src='Files/int.JPG' width='290px' height='auto'/>
         <h6 className={intVisibility ? 'left ab text-box visible' : 'left ab text-box unvisible'}>
         Outside of school I enjoy spending my time lifting, cycling, video-gaming, woodworking, traveling, and simply hanging out with
         friends/family. I also enjoy coding(hence my major) and like to learn new technologies for building various applications.
